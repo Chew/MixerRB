1b4e980334f88926c498fa66e4ff8a12630cf895
Chewsterchew <chew@chew.pw>
2018-04-29 21:15:31 -0500

    Don't get achievements on load, that's silly.

--- lib/mixerrb/achievements.rb
+++ lib/mixerrb/achievements.rb
@@ -2,17 +2,19 @@
 class MixerRB::Achievements
   # Initialize the stats
   # Program does this automatically when running MixerRB.new.
-  def initialize
+  def initialize; end
+
+  # Get the achievement list.
+  def load
     @data = JSON.parse(RestClient.get('https://mixer.com/api/v1/achievements'))
   end
 
-  # Update the achievement list, you rarely ever need to this, however.
-  def update
-    @data = JSON.parse(RestClient.get('https://mixer.com/api/v1/achievements'))
-  end
+  alias update load
 
   # Raw Data in JSON form.
   attr_reader :data
+
+  alias to_s data
 
   # Amount of achievements.
   # @return [Integer] amount of achievements.
